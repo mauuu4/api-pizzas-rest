35a6090502ead8eb49d38ffaf06b7146d27b067d
mauu <mauriciord2004@gmail.com>
2025-05-17 23:11:48 -0500

    Add dotenv for environment variable management and update scripts

--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export const DB_USER = process.env.DB_USER
 export const DB_PASSWORD = process.env.DB_PASSWORD
 export const DB_HOST = process.env.DB_HOST
